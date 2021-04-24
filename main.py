13dac8dd43fe12160b0265311bd4ac17dc856438
Rafal Przetakowski <rafal.p@beeflow.co.uk>
2021-04-24 12:33:18 +0100

    fixed part 1

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 """copyright (c) 2021 Beeflow Ltd.
 
 Author Rafal Przetakowski <rafal.p@beeflow.co.uk>"""
+from models.book_copy import Status
 from models.first_name import FirstName
 from models.user import User
 
 if __name__ == '__main__':
-    # statuses = Status.select().filter(name="wypożyczona").execute()
-    # for status in statuses:
-    #     for book in status.books:
-    #         print(book)
+    statuses = Status.select().filter(name="wypożyczona").execute()
+    for status in statuses:
+        for book in status.books:
+            print(book)
 
     names = FirstName.select().filter(name="rafał").execute()
     for name in names:
