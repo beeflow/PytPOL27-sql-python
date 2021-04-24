ec12d1d7bd0ec557d4c5865b6321cb4d57545470
Rafal Przetakowski <rafal.p@beeflow.co.uk>
2021-04-24 10:49:36 +0100

    first and last name

---
+++ models/author.py
@@ -0,0 +1,17 @@
+"""copyright (c) 2021 Beeflow Ltd.
+
+Author Rafal Przetakowski <rafal.p@beeflow.co.uk>"""
+from peewee import *
+
+from common.base_model import BaseModel
+from models.first_name import FirstName
+from models.last_name import LastName
+
+
+class Author(BaseModel):
+    id = AutoField(column_name="author_id")
+    first_name = ForeignKeyField(FirstName, column_name="first_name_id")
+    last_name = ForeignKeyField(LastName, column_name="last_name_id")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- models/book.py
+++ models/book.py
@@ -4,6 +4,7 @@ Author Rafal Przetakowski <rafal.p@beeflow.co.uk>"""
 from peewee import *
 
 from common.base_model import BaseModel
+from models.author import Author
 
 
 class Book(BaseModel):
@@ -12,6 +13,7 @@ class Book(BaseModel):
     isbn = CharField(max_length=13, null=True, column_name="book_isbn")
     pages = IntegerField(column_name="book_pages", null=True)
     published_on_year = IntegerField(column_name="book_publish_year", null=True)
+    authors = ManyToManyField(Author, backref="books")
 
     def __str__(self):
         return self.title
