39aa696baabe4ea3e34ae6c11c9b0df56b571f19
Rafal Przetakowski <rafal.p@beeflow.co.uk>
2021-04-24 10:36:00 +0100

    first and last name

---
+++ models/first_name.py
@@ -0,0 +1,13 @@
+"""copyright (c) 2021 Beeflow Ltd.
+
+Author Rafal Przetakowski <rafal.p@beeflow.co.uk>"""
+from peewee import *
+
+from common.base_model import BaseModel
+
+
+class FirstName(BaseModel):
+    name = CharField(max_length=15, column_name="first_name")
+
+    def __str__(self):
+        return self.name
---
+++ models/last_name.py
@@ -0,0 +1,13 @@
+"""copyright (c) 2021 Beeflow Ltd.
+
+Author Rafal Przetakowski <rafal.p@beeflow.co.uk>"""
+from peewee import *
+
+from common.base_model import BaseModel
+
+
+class LastName(BaseModel):
+    name = CharField(max_length=50, column_name="last_name")
+
+    def __str__(self):
+        return self.name
