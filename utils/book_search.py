827d92bee3ca09f4a5540ea46de65983a09e77ad
Rafal Przetakowski <rafal.p@beeflow.co.uk>
2021-05-15 10:37:43 +0100

    search fixed

--- utils/book_search.py
+++ utils/book_search.py
@@ -1,6 +1,8 @@
 """copyright (c) 2021 Beeflow Ltd.
 
 Author Rafal Przetakowski <rafal.p@beeflow.co.uk>"""
+from peewee import JOIN
+
 from models.author import Author
 from models.book import Book
 from models.first_name import FirstName
@@ -8,7 +10,19 @@ from models.last_name import LastName
 
 
 def find_book(search_param):
-    # @todo FIXME :P
+    def where(result, first_name, last_name):
+        if first_name:
+            return result.where(
+                (Book.title == search_param) | (Author.first_name == first_name)
+            )
+
+        if last_name:
+            return result.where(
+                (Book.title == search_param) | (Author.last_name == last_name)
+            )
+
+        return result.where(Book.title == search_param)
+
     BookAuthor = Book.authors.get_through_model()
 
     # 1. zapytanie o nazwisko
@@ -24,8 +38,7 @@ def find_book(search_param):
         first_name = None
 
     # 3. zapytanie o książkę o tytule lub autorze
-    return Book.select().left_outer_join(BookAuthor).left_outer_join(Author).where(
-        (Book.title == search_param) | (
-                (Author.first_name == first_name) | (Author.last_name == last_name)
-        )
-    )
+    result = Book.select().join(BookAuthor, JOIN.LEFT_OUTER).join(Author, JOIN.LEFT_OUTER)
+    result = where(result, first_name, last_name)
+
+    return result
