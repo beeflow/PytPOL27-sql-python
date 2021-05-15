f0ad92a9095100f5eff95655c1d00c6871b83835
Rafal Przetakowski <rafal.p@beeflow.co.uk>
2021-05-15 09:59:47 +0100

    search fix

--- utils/book_search.py
+++ utils/book_search.py
@@ -3,11 +3,22 @@
 Author Rafal Przetakowski <rafal.p@beeflow.co.uk>"""
 from models.author import Author
 from models.book import Book
+from models.first_name import FirstName
+from models.last_name import LastName
 
 
 def find_book(search_param):
     # @todo FIXME :P
     BookAuthor = Book.authors.get_through_model()
+
+    # 1. zapytanie o nazwisko
+    LastName.select().where(LastName.name == "King")
+
+    # 2. zapytanie o imię
+
+    # 3. zapytanie o autora o imieniu lub nazwisku
+
+    # 4. zapytanie o książkę o tytule lub autorze
 
     return Book.select().join(BookAuthor).join(Author).where(
         (Book.title == search_param) | (
